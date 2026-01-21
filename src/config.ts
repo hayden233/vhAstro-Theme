0b37a8b1847cdbc49e52fee4ab1ae2b6e69a1459
Hayden <2895612785@qq.com>
2026-01-21 21:52:14 +0800

    更改头像

--- src/config.ts
+++ src/config.ts
@@ -10,7 +10,7 @@ export default {
   // 网站作者
   Author: 'Hayden',
   // 作者头像
-  Avatar: 'https://q1.qlogo.cn/g?b=qq&nk=1655466387&s=640',
+  Avatar: 'https://q1.qlogo.cn/g?b=qq&nk=2895612785&s=640',
   // 网站座右铭
   Motto: '运气是计划之外的东西.',
   // Cover 网站缩略图
